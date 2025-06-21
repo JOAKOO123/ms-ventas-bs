0786cd8a16b8320bf62d3572fde010772e924373
fernang0 <fer.henriquez248@gmail.com>
2025-06-21 01:45:21 -0400

    nosekemas

--- src/main/java/cl/duoc/bs_ventas_bs/model/dto/ClienteDTO.java
+++ src/main/java/cl/duoc/bs_ventas_bs/model/dto/ClienteDTO.java
@@ -1,5 +1,28 @@
 package cl.duoc.bs_ventas_bs.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class ClienteDTO {
 
+    private Long id;
+
+    private String nombre;
+
+    private String contrasena;
+
+    private String direccion;
+
+    private String email;
+
+    private String telefono;
 }
